e3a50cc89c136f335a2aa9bb5840bf9c25e44699
Pham99 <tomas.phamt@gmail.com>
2024-05-05 16:10:51 +0200

    Added Color Picker and Layout

--- KanbanBox.cs
+++ KanbanBox.cs
@@ -1,9 +1,11 @@
-﻿namespace BlazorAppEmpty
+﻿using System.Drawing;
+
+namespace BlazorAppEmpty
 {
 	public class KanbanBox
 	{
 		public string? Title { get; set; }
-
+		public string? BoxColor { get; set; }
 		public List<KanbanItem> kanbanItems { get; set; } = new List<KanbanItem>();
 
 		public void AddItem(string text)
