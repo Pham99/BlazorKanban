27e8e5732e9a57ade94f71b83b5448fd4a7d3290
Pham99 <tomas.phamt@gmail.com>
2024-06-22 03:10:30 +0200

    lots of random shit

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 namespace BlazorAppEmpty.Models
 {
@@ -6,8 +7,14 @@ namespace BlazorAppEmpty.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Musí tam být jméno!")]
+        [StringLength(50)]
         public string Name { get; set; }
+        [EmailAddress]
+        [StringLength(50)]
         public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Password { get; set; }
     }
 }
